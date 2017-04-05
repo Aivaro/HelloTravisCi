722d92c008f8ebd14c97e36cfb7f093c55f07756
Aivaro <aivaras.silale@gmail.com>
2017-04-06 00:47:54 +0300

    Initial commit

---
+++ src/main/java/HelloTravis.java
@@ -0,0 +1,10 @@
+/**
+ * Created by Vavukas on 4/6/2017.
+ */
+public class HelloTravis {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World, I'm using Travis-CI!");
+    }
+
+}
