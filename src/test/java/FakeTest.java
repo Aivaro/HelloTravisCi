d6409b4f8b88005641d64a94a809949b0bb9709f
Aivaro <aivaras.silale@gmail.com>
2017-04-06 00:55:08 +0300

    added fake test

---
+++ src/test/java/FakeTest.java
@@ -0,0 +1,15 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Created by Vavukas on 4/6/2017.
+ */
+public class FakeTest {
+
+@Test
+public void simpleTest() {
+    int vienas = 1;
+    Assert.assertEquals(1, vienas);
+}
+
+}
