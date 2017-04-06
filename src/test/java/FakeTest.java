fb89a99cb75e64abc232bd05d41b7e1fae8952f8
Aivaras Silale <aisi0860@slyva04.mif>
2017-04-06 16:42:04 +0300

    xx

--- src/test/java/FakeTest.java
+++ src/test/java/FakeTest.java
@@ -11,13 +11,13 @@ public void simpleTest() {
     int vienas = 1;
     Assert.assertEquals(1, vienas);
 }
-
-@Test
-public void lambadaTest(){
-    Runnable runnable2 = () -> { System.out.println("Running from Lambda"); };
-
-
-
-}
+//
+//@Test
+//public void lambadaTest(){
+//    Runnable runnable2 = () -> { System.out.println("Running from Lambda"); };
+//
+//
+//
+//}
 
 }
